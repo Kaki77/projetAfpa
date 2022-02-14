5677ca7c93a01c26d51c1f3b203b85c754bc6e1d
Julien ROUX <j.roux@my-groom-service.com>
2022-02-14 14:11:09 +0100

    init projet

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Validator;
+
+class AuthController extends Controller
+{
+    public function login(Request $request){
+        $credentials=$request->all();
+        $validator=Validator::make($credentials,[
+            'email'=>['required','email'],
+            'password'=>['required']
+        ]);
+        if($validator->fails()){
+            return $this->handleError($validator->errors());
+        }
+        if(Auth::attempt($credentials)){
+            $request->session()->regenerate();
+
+            return $this->handleResponse([],'User logged-in');
+        }
+        else{
+            $this->handleError('Credentials do not match');
+        }
+    }
+
+    public function logout(Request $request){
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return $this->handleResponse([],'Logout');
+    }
+
+    public function register(Request $request){
+        $input=$request->all();
+        $validator=Validator::make($input,[
+            'name'=>['required'],
+            'email'=>['required','email'],
+            'password'=>['required'],
+            'confirm_password'=>['required','same:password']
+        ]);
+        if($validator->fails()){
+            return $this->handleError($validator->errors());
+        }
+
+        $input['password']=bcrypt($input['password']);
+        $user=User::create($input);
+
+        return $this->handleResponse($user,'User registered');
+    }
+
+}
